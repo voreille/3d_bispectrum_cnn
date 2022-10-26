6e044160a2aee9aeb1f629e7ce3eee8833f2f19e
v-andrearczyk <vincent.andrearczyk@gmail.com>
2022-10-26 14:00:09 +0200

    update

--- src/scripts/repeated_training.sh
+++ src/scripts/repeated_training.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for i in {0..9}
+# {0..9}
+for i in 0 
 do
-   docker exec -w /workspaces/3d_bispectrum_cnn -u vscode cool_poitras python src/models/train_model.py --gpu-id ${2} --split-id $i --config ${1} --memory-limit ${3}
+   python src/models/train_model.py --gpu-id ${2} --split-id $i --config ${1} --memory-limit ${3}
 done
